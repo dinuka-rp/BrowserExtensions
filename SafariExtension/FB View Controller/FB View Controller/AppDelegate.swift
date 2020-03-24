39f81614fe0e565172a8ae2654d53bbdb185fddb
DinDev3 <drpiyadigama@gmail.com>
2020-03-24 11:36:01 +0530

    Created a browser extension for Safari

---
+++ SafariExtension/FB View Controller/FB View Controller/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  FB View Controller
+//
+//  Created by Dinuka Piyadigama on 3/24/20.
+//  Copyright © 2020 Dinuka Piyadigama. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+}
